f88a2411017db075024129baefc84f26927d71ec
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-04-07 12:32:03 +0530

    Initial Commit

---
+++ DemoIosApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoIosApp
+//
+//  Created by Bhumika Patel on 07/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoIosApp/DemoIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoIosAppApp.swift
+//  DemoIosApp
+//
+//  Created by Bhumika Patel on 07/04/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
